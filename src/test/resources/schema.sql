b98afc8635a9ad051a9c46d99887d2123e4021c5
Tarek Marjieh <marjieh.tarek.98@gmail.com>
2025-03-21 02:51:37 +0200

    Built the yaml and yml files for the connection of the database of the tests.

---
+++ src/test/resources/schema.sql
@@ -0,0 +1,32 @@
+CREATE TABLE IF NOT EXISTS task (
+    description VARCHAR(64) NOT NULL,
+    completed   VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS movie (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) UNIQUE,
+    genre VARCHAR(255),
+    duration DOUBLE PRECISION,
+    rating DOUBLE PRECISION,
+    release_year INT
+);
+
+CREATE TABLE IF NOT EXISTS showtime (
+    id SERIAL PRIMARY KEY,
+    movieId INT,
+    theater VARCHAR(255),
+    startTime TIMESTAMPTZ,
+    endTime TIMESTAMPTZ,
+    price DOUBLE PRECISION,
+    CONSTRAINT fk_movie FOREIGN KEY (movieId) REFERENCES movie(id)
+);
+
+CREATE TABLE IF NOT EXISTS ticket (
+    bookingId UUID,
+    showtimeId INT,
+    seatNumber INT,
+    userId UUID,
+    CONSTRAINT fk_showtime FOREIGN KEY (showtimeId) REFERENCES Showtime(id),
+    CONSTRAINT unique_seat_showtime UNIQUE (seatNumber, showtimeId)
+);
